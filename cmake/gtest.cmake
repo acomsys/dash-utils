315c64c4671f21db73fc58563146838ba7e25a98
gil.regalado <gil.regalado@netzontech.com>
2022-08-21 20:08:42 +0800

    chore(ci): cmake refactor

--- cmake/gtest.cmake
+++ cmake/gtest.cmake
@@ -11,17 +11,3 @@ if(WIN32)
 endif()
 
 FetchContent_MakeAvailable(googletest)
-
-enable_testing()
-
-add_executable(
-  hello_test
-  src/native/hello/hello_test.cc
-)
-target_link_libraries(
-  hello_test
-  GTest::gtest_main
-)
-
-include(GoogleTest)
-gtest_discover_tests(hello_test)
