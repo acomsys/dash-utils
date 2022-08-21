697938c0ae36a24db4263a3790a36079212a023c
bangonkali <bangonkali@gmail.com>
2022-08-21 15:37:14 +0000

    chore(ci): memoize module loading

--- src/loader.ts
+++ src/loader.ts
@@ -7,11 +7,15 @@ const paths = [
     `../build/Release`,
 ]
 
+let module: any | undefined = undefined;
+
 export const load = <T>(name: string) => {
-    const bindingPath = paths
-        .map((p) => path.resolve(__dirname, p, name))
-        .filter(p => fs.existsSync(`${p}.node`))[0]
-    console.log(`bindingPath: ${bindingPath}`)
-    const module = require(bindingPath);
+    if (module === undefined) {
+        const bindingPath = paths
+            .map((p) => path.resolve(__dirname, p, name))
+            .filter(p => fs.existsSync(`${p}.node`))[0]
+        console.log(`bindingPath: ${bindingPath}`)
+        module = require(bindingPath);
+    }
     return module as T;
 }
